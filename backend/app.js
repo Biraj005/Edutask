ab61541d2e697cf645e119f994db75abfa674bc2
Biraj Roy <birajroy0007@gmail.com>
2025-11-07 23:22:32 +0530

    deployment fix

--- backend/app.js
+++ backend/app.js
@@ -5,25 +5,36 @@ import taskRouter from "./Routes/tasks.route.js";
 import subjectRouter from "./Routes/subject.route.js";
 import { connectDb } from "./util/db.js";
 import cookieParser from "cookie-parser";
-import cors from 'cors'
+import cors from "cors";
+import path from "path";
 
 const app = express();
 
+const __dirname = path.resolve();
+
+if(process.env.NODE_ENV!=="production"){
+    app.use(
+      cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+      })
+    );
+}
 
 
-app.use(cors({
-  origin: "http://localhost:5173", 
-  credentials: true               
-}));
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 app.use(subjectRouter);
 
-app.get("/", (req, res) => {
-  res.send("Hiii");
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 3000;
 connectDb();
 app.listen(PORT, () => {

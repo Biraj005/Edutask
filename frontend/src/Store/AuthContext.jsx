8642e118379f329ffb0494e0de438d96787c97df
Biraj Roy <birajroy0007@gmail.com>
2025-11-07 23:58:16 +0530

    deployment bug

--- frontend/src/Store/AuthContext.jsx
+++ frontend/src/Store/AuthContext.jsx
@@ -4,7 +4,10 @@ import { toast } from "react-toastify";
 
 export const AuthContext = createContext(null);
 
-const backendUrl = import.meta.env.VITE_BACKEND_URL;
+const backendUrl = import.meta.env.NODE_ENV === 'production'
+  ? '/api'
+  : import.meta.env.VITE_BACKEND_URL;
+
 
 const AuthContextProvider = ({ children }) => {
 
